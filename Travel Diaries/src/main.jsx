0ad45f0d3b43408177ab29e13557b8f6be01df8b
amangupta776 <138656013+amangupta776@users.noreply.github.com>
2024-03-02 12:59:22 +0530

    Making Som changes

--- Travel Diaries/src/main.jsx
+++ Travel Diaries/src/main.jsx
@@ -4,6 +4,6 @@ import ReactDOM from 'react-dom/client'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <h1>Hello</h1>
+  <h1>Hello world</h1>
   </React.StrictMode>,
 )
