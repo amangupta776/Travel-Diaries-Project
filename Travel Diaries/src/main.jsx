5716371c3019ad5b8428b3f48d59b2a48d646a36
deepaksinghh13 <151deepaksss@gmail.com>
2024-03-02 22:32:30 +0530

    Changes

--- Travel Diaries/src/main.jsx
+++ Travel Diaries/src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 import Routing from './Routing.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <h1>HELLO</h1>
     <Routing/>
   </React.StrictMode>,
 )
