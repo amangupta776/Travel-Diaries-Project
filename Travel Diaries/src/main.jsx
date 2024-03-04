058079a2cf812d9af7d8ababe3e044bd243bf7ad
deepaksinghh13 <151deepaksss@gmail.com>
2024-03-04 22:49:56 +0530

    checking something

--- Travel Diaries/src/main.jsx
+++ Travel Diaries/src/main.jsx
@@ -6,6 +6,7 @@ import Routing from './Routing.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <h1>HELLO</h1>
+    <h2>Hi edit by deepak side</h2>
     <Routing/>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
